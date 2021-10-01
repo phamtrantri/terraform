06e2fce4b6804693f41b9d3bd13e4822fa1b731d
Tri Pham <tri.pham@skedulo.com>
2021-10-01 14:54:46 +0700

    init

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+  default = "ap-southeast-2"
+}
+
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+      "ap-southeast-2" = "ami-0e554a91eb4e7b6d7"
+  }
+}
