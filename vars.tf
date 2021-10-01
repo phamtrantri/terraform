20949ee5184241452f6786c64e4eedfb8ffbac67
Tri Pham <tri.pham@skedulo.com>
2021-10-01 17:30:59 +0700

    run command on remote

--- vars.tf
+++ vars.tf
@@ -11,3 +11,14 @@ variable "AMIS" {
       "ap-southeast-2" = "ami-0e554a91eb4e7b6d7"
   }
 }
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
